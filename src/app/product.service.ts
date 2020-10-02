19617d6e3cb16b26b99f0964278227d752426231
unassuminglily <oforiamanfo.lilyaa@gmail.com>
2020-10-02 23:02:04 +0000

    mod6 done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,14 @@ import 'rxjs/add/operator/map';
 import { from } from 'rxjs/observable/from';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -16,5 +19,10 @@ export class ProductService {
     return this._http.get(this._albumUrl)
     .map((response) => <Album>response.json());
   }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get(this._productsUrl)
+    .map((response) => <Product[]>response.json());
+  };
 
 }
